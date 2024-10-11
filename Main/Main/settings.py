8d68503e49853187f5b5c55c695bcd26871c5f49
MD MUSA ALAM <mdmusaalam98019@gmail.com>
2024-10-11 23:36:51 +0530

    initial commit

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -233,3 +233,7 @@ REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
 #     'PAGE_SIZE':1
 # }
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
